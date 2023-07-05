ef5b0fda87f6be1dd42c924d2bd2f5fadc523a6a
iCyuba <48867623+iCyuba@users.noreply.github.com>
2023-07-05 15:54:43 +0200

    Also updated the modals
    I'll probably work on the actual themes now..
    We'll see....

--- src/components/Modal.svelte
+++ src/components/Modal.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
     import Close from "@material-design-icons/svg/filled/close.svg?component";
 
+    import styles from "$styles/modal.module.scss";
+
     /** Whether the modal is visible */
     export let visible: boolean;
 
@@ -24,12 +26,12 @@
 </script>
 
 <!-- svelte-ignore a11y-click-events-have-key-events a11y-no-noninteractive-element-interactions -->
-<dialog class="modal" class:scrollable bind:this={dialog} on:close={hide} on:click|self={hide}>
-    <button class="close-button" on:click={hide}>
+<dialog class={styles.modal + (scrollable ? ` ${styles.scrollable}` : "")} bind:this={dialog} on:close={hide} on:click|self={hide}>
+    <button class={styles.closeButton} on:click={hide}>
         <Close />
     </button>
 
-    <div class="modal-content">
+    <div class={styles.content}>
         <slot />
     </div>
 </dialog>
