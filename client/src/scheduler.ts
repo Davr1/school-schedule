5aa3fd5596f129c00c84f3e5f5801302e3bfe8f7
iCyuba <48867623+iCyuba@users.noreply.github.com>
2023-09-20 20:26:28 +0200

    More frequent runs... Just testing something, ignore it..

--- client/src/scheduler.ts
+++ client/src/scheduler.ts
@@ -14,10 +14,14 @@ async function scheduleAllClasses() {
     log("Scheduling task: AllClasses");
 
     // Skip Saturdays because there's no school. (Sundays are fetched anyway for archival)
-    const task = new Cron("0 30 23 * * 0-5", { name: "AllClasses", timezone: "Europe/Prague" }, async () => {
+    const task = new Cron("0 0 */4,23 * * 0-5", { name: "AllClasses", timezone: "Europe/Prague" }, async () => {
         // Log the run time and the next run time
         log(`Running task "AllClasses" at ${new Date().toLocaleString()}`);
         log(` - Next run time for task "AllClasses": ${task.nextRun()?.toLocaleString()}`);
+
+        // Wait 5 to 15 minutes
+        const min = Math.random() * 10 + 5;
+        await new Promise((resolve) => setTimeout(resolve, min * 60 * 1000));
 
         // Fetch the schedules.
         const schedules = await fetchAllClasses(Week.Current);
