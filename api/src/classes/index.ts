115b1c969d7cc940c6f61e67dc6cdd9516fc3b9e
iCyuba <48867623+iCyuba@users.noreply.github.com>
2023-10-01 13:44:56 +0200

    Cleanup
    Added some index files for easier access...

---
+++ api/src/classes/index.ts
@@ -0,0 +1,8 @@
+// Bakalari classes
+export { default as Bakalari, BakalariType } from "@/classes/bakalari";
+export { default as BakalariDay, type BakalariPeriod } from "@/classes/bakalari/day";
+export * from "@/classes/bakalari/lesson";
+
+// SSSVT classes
+export { default as SSSVT, type Period } from "@/classes/sssvt";
+export * from "@/classes/sssvt/change";
---
+++ api/src/parser/index.ts
@@ -0,0 +1,3 @@
+// Parser index file
+export { default as parseBakalari } from "@/parser/bakalari";
+export { default as parseSSSVT } from "@/parser/sssvt";
