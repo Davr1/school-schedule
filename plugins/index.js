13874558defbacf84eed042782fbd1ffb71b4689
iCyuba <48867623+iCyuba@users.noreply.github.com>
2024-01-18 21:12:11 +0100

    Replace build generation script with a custom Vite plugin The current pre-build script generates a Sass stylesheet with all the available colors, which are then used to generate the given classes. However, this adds a slow generation step that must run before the build.
    This change includes a custom Vite plugin that injects the generated color variables into the global CSS. Unlike the previous solution, this happens during the build and thus adds basically no time penalty.

---
+++ plugins/index.js
@@ -0,0 +1,62 @@
+import tailwindColors from "tailwindcss/colors.js";
+
+/** Deprecated colors on the exported tailwind object */
+const deprecated = ["lightBlue", "warmGray", "trueGray", "coolGray", "blueGray"];
+
+// Generate the CSS for the colors
+const shades = ["400", "500", "600"];
+const bgShades = ["50", "100", "200", "400", "500", "600", "700", "800", "900", "950"];
+const bgColors = ["slate", "gray", "zinc", "neutral", "stone"];
+
+/**
+ * Genrates the CSS variable for a color shade
+ * @param {string} color Name of the tailwind color
+ * @param {string} name Name of the CSS variable
+ * @param {string[]} shades Shades to generate
+ * @returns Generated CSS variable
+ */
+function variablesForColorShade(color, name, shades) {
+    return shades.map((shade) => `--${name}-${shade}: ${tailwindColors[color][shade]};`).join("");
+}
+
+// Generate the CSS for the colors
+let css = "";
+for (const color of Object.keys(tailwindColors)) {
+    // Ignore deprecated colors / invalid colors
+    if (deprecated.includes(color) || typeof tailwindColors[color] !== "object") continue;
+
+    // Primary and secondary color classes
+    css += `
+        .${color} {
+            ${variablesForColorShade(color, "primary", shades)}
+        }
+
+        .${color}-2 {
+            ${variablesForColorShade(color, "secondary", shades)}
+        }
+    `;
+
+    // Background color classes (only for certain colors)
+    if (!bgColors.includes(color)) continue;
+
+    css += `
+        .${color}-bg {
+            ${variablesForColorShade(color, "background", bgShades)}
+        }
+    `;
+}
+
+/**
+ * A custom Vite plugin that adds CSS colors to the global stylesheet
+ * @returns {import("vite").Plugin}
+ */
+export function customCssColorsPlugin() {
+    return {
+        name: "custom-css-colors-plugin",
+        apply: "build",
+        transform(code, id) {
+            // Inject the custom CSS colors into the global styles
+            if (id.endsWith("global.scss")) return css + code;
+        }
+    };
+}
