8d938667499b35712ef78e38f35ded643f3f63b5
iCyuba <48867623+iCyuba@users.noreply.github.com>
2024-02-01 21:38:00 +0100

    Update sssvt parser

--- api/src/parser/sssvt/lesson.ts
+++ api/src/parser/sssvt/lesson.ts
@@ -17,67 +17,55 @@ class SSSVTLessonParser {
         // Check if the lesson has any text inside, if not, return
         if (!lesson.textContent?.trim()) return;
 
-        // Get the group number and subject abbreviation
-        const group = this.#group(lesson) ?? null;
-        const subjectAbbreviation = this.#subject(lesson) ?? null;
+        // Get the group number and subject
+        const group = this.#group(lesson);
+        const subject = this.#subject(lesson);
 
         // If there's no subject, it means that the lesson is cancelled
-        // Note: Only check for null because the value can be an empty string
-        if (subjectAbbreviation === null) return new SSSVTCancellation(group);
+        // Note: Only check for undefined because the value can be null (which means that the subject wasn't specified)
+        if (subject === undefined) return new SSSVTCancellation(group);
 
-        // Find the subject detail (or add it if it doesn't exist)
-        const subject = subjectAbbreviation
-            ? this.#details.get(subjectAbbreviation, () => new Detail(DetailType.Subject, subjectAbbreviation, null))
-            : null;
-
-        // Get the room number and teacher abbreviation
-        const room = this.#room(lesson) ?? null;
-        const teacherAbbreviation = this.#teacher(lesson) ?? null;
-
-        // Find the teacher detail (or add it if it doesn't exist)
-        const teacher =
-            teacherAbbreviation !== null
-                ? this.#details.getByAbbreviation<TeacherDetail>(
-                      teacherAbbreviation,
-                      () => new TeacherDetail(teacherAbbreviation, null, teacherAbbreviation)
-                  )
-                : null;
-
-        // If there's no room, throw an error
-        if (!room) throw new Error(`Couldn't find the room in lesson: ${lesson.textContent}`);
+        // Get the room number and teacher
+        const room = this.#room(lesson);
+        const teacher = this.#teacher(lesson) ?? null;
 
         return new SSSVTSubstitution(group, subject, teacher, room);
     }
 
     /** Parse the group number from the given lesson node */
-    #group(lesson: IElement): number | undefined {
+    #group(lesson: IElement): number | null {
         // Parse the group number from the lesson content in the format: (N.sk)
         const match = lesson.textContent?.match(/(?<=\()[0-9](?=\.sk\))/)?.[0];
 
         // If it exists, return it as a number
         if (match) return parseInt(match);
+        return null;
     }
 
     /** Parse the subject abbreviation from a lesson */
-    #subject(lesson: IElement): string | undefined {
+    #subject(lesson: IElement): Detail | null | undefined {
         // Get the subject abbreviation from the lesson
-        const subject = lesson.querySelector("strong");
+        // The subject abbreviation is in the text content of the <strong> tag
+        let subject = lesson.querySelector("strong")?.textContent?.trim();
 
-        // The subject is in the text content of the <strong> tag
-        const name = subject?.textContent?.trim();
+        if (subject === undefined) return; // Undefined => cancelled lesson
+        if (subject === "") return null; // Empty string => no subject specified
 
         // If the subject is "POS", replace it with "SPS" (it's the same subject)
-        return name === "POS" ? "SPS" : name;
+        if (subject === "POS") subject = "SPS";
+
+        // Find the subject detail (or add it if it doesn't exist)
+        return this.#details.get(subject, () => new Detail(DetailType.Subject, subject!, null));
     }
 
     /** Parse the room for the given lesson */
-    #room(lesson: IElement): Detail | undefined {
+    #room(lesson: IElement): Detail {
         // Get the room number from the <a> tag with an href attribute
         const link = lesson.querySelector("[href*='/room/']");
 
         // The room name is in the text content of the link
         const name = link?.textContent?.trim();
-        if (!name) return;
+        if (!name) throw new Error("Couldn't find the room in lesson");
 
         // Find the room detail (or add it if it doesn't exist)
         const room = this.#details.getByName(name, () => new Detail(DetailType.Room, name, name));
@@ -86,12 +74,14 @@ class SSSVTLessonParser {
     }
 
     /** Parse the teacher's abbreviation from a lesson */
-    #teacher(lesson: IElement): string | undefined {
+    #teacher(lesson: IElement): TeacherDetail | null {
         // Get the teacher's abbreviation from the link
-        const teacher = lesson.querySelector("[href*='/teacher/']");
-
         // The teacher abbreviation is in the text content of the <em> tag
-        return teacher?.textContent?.trim();
+        const teacher = lesson.querySelector("[href*='/teacher/']")?.textContent?.trim();
+        if (!teacher) return null;
+
+        // Find the teacher detail (or add it if it doesn't exist)
+        return this.#details.getByAbbreviation<TeacherDetail>(teacher, () => new TeacherDetail(teacher, teacher, null));
     }
 }
 
